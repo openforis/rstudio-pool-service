9915f276e2c73a1ea681424041ce0c3af22e8280
ramirobg94 <ramirobg94@gmail.com>
2021-07-26 11:15:46 +0200

    add remove

--- RStudioPoolService/src/useCases/checkInstances/index.js
+++ RStudioPoolService/src/useCases/checkInstances/index.js
@@ -6,27 +6,19 @@ const { Manager: InstanceManager } = Instance
 const checkInstances = async (event, { payload = {} } = {}) => {
   const { userId } = payload
 
-  let responseData = {}
-
   if (!userId || !Authorizer(event, [ARENA_POOL_SERVICE_KEY])) {
-    const response = {
+    return {
       statusCode: 403,
-      body: JSON.stringify(responseData),
+      body: JSON.stringify({}),
     }
-    return response
   }
 
-  if (userId) {
-    const instance = await InstanceManager.getInstanceByUserId({ userId })
-    responseData = { instance, rStudioProxyUrl: ARENA_PROXY_URL }
-  }
+  const instance = await InstanceManager.getInstanceByUserId({ userId })
 
-  const response = {
+  return {
     statusCode: 200,
-    body: JSON.stringify(responseData),
+    body: JSON.stringify({ instance, rStudioProxyUrl: ARENA_PROXY_URL }),
   }
-
-  return response
 }
 
 module.exports = checkInstances
--- RStudioPoolService/src/useCases/removeInstance/index.js
+++ RStudioPoolService/src/useCases/removeInstance/index.js
@@ -1,17 +1,28 @@
-const { PROXY_KEY } = require('../../../config')
+const { PROXY_KEY, ARENA_POOL_SERVICE_KEY } = require('../../../config')
 const { Instance, Authorizer } = require('../../domain')
 
-const { Manager: InstanceManager } = Instance
+const { Manager: InstanceManager, Model: InstanceModel } = Instance
 
 const removeInstance = async (event, { payload = {} } = {}) => {
-  const { instanceId } = payload
+  const { instanceId, userId } = payload
 
-  if (!Authorizer(event, [PROXY_KEY])) {
+  if (!Authorizer(event, [PROXY_KEY, ARENA_POOL_SERVICE_KEY])) {
     const response = {
       statusCode: 403,
       body: JSON.stringify({}),
     }
     return response
+  }
+
+  if (Authorizer(event, [ARENA_POOL_SERVICE_KEY])) {
+    const instance = InstanceManager.getInstanceByUserId({ userId })
+    if (InstanceModel.getId(instance) !== instanceId) {
+      const response = {
+        statusCode: 403,
+        body: JSON.stringify({}),
+      }
+      return response
+    }
   }
 
   await InstanceManager.terminateInstance({ instanceId })
