789ef6ef47d7edece0ba932025cb7cafe76210ed
Stefano Ricci <SteRiccio@users.noreply.github.com>
2025-06-29 00:49:09 +0200

    fixed Docker installation

--- RStudioPoolService/src/domain/Instance/model.js
+++ RStudioPoolService/src/domain/Instance/model.js
@@ -58,15 +58,16 @@ const getNewInstanceConfig = ({ userId = false } = {}) => ({
     cd /home/ubuntu/docker-runner
     sudo chown -R $USER:$USER /home/ubuntu/docker-runner
 
+    # Add Docker's official GPG key:
     sudo apt-get update
     sudo apt-get install ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
 
     # Add the repository to Apt sources:
     echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
       $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
@@ -79,7 +80,12 @@ const getNewInstanceConfig = ({ userId = false } = {}) => ({
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     # install AWS CLI
-    sudo apt install awscli -y
+    sudo apt-get install -y unzip
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+    rm -r aws
+    rm awscliv2.zip
 
     # login to Amazon ECR
     aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACCOUNT}
