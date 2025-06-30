420b0eed94f30e0ae333783780a43f0e9e26460d
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2025-06-30 10:40:23 +0200

    Docker RStudio instance: always restart it if stopped

--- RStudioPoolService/src/domain/Instance/model.js
+++ RStudioPoolService/src/domain/Instance/model.js
@@ -92,7 +92,7 @@ const getNewInstanceConfig = ({ userId = false } = {}) => ({
 
     # pull rstudio image
     docker pull ${ACCOUNT}/rstudio
-    sudo docker run -d -p 8787:8787 -e DISABLE_AUTH=true ${ACCOUNT}/rstudio
+    sudo docker run -d -p 8787:8787 -e DISABLE_AUTH=true --restart always ${ACCOUNT}/rstudio
  `,
   TagSpecifications: [
     {
